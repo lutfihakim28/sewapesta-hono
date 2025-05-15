5c213dc78dcfb810ae5666402c67232d44c88056
Lutfi <lutfihakim288@gmail.com>
2025-05-15 19:47:15 +0700

    better logging

--- db/index.ts
+++ db/index.ts
@@ -6,6 +6,13 @@ const connection = new Database('sewapesta.db')
 export const db = drizzle(connection, {
   logger: {
     logQuery: (query, params) => {
+      if (query.toLowerCase().includes('create table')) return;
+      if (query.toLowerCase().includes('create index')) return;
+      if (query.toLowerCase().includes('create unique index')) return;
+      if (query.toLowerCase().includes('insert into \"provinces\"')) return;
+      if (query.toLowerCase().includes('insert into \"cities\"')) return;
+      if (query.toLowerCase().includes('insert into \"districts\"')) return;
+      if (query.toLowerCase().includes('insert into \"subdistricts\"')) return;
       logger.info({ sql: query, params }, 'Executing SQL Query');
     },
   },
