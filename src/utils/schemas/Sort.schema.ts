7c539d807bb7067817c4d4351d4a05b2be58fade
Lutfi Hakim <lutfihakim288@gmail.com>
2025-05-27 13:42:38 +0700

    ~Fix sort schema, support single element of array

--- src/utils/schemas/Sort.schema.ts
+++ src/utils/schemas/Sort.schema.ts
@@ -1,16 +1,19 @@
 import { ObjectSchema } from './Object.schema';
 import { StringSchema } from './String.schema';
 import { ArraySchema } from './Array.schema';
+import { UnionSchema } from './Union.schema';
 
 export function SortSchema<T extends string>(columns: T[]) {
   return new ObjectSchema({
-    asc: new ArraySchema('asc', new StringSchema('asc').getSchema()).getSchema()
+    asc: new UnionSchema([new ArraySchema('asc', new StringSchema('asc').getSchema()).getSchema()
       .optional()
-      .transform((val) => !val ? [] : val)
+      .transform((val) => !val ? [] : val), new StringSchema('asc').getSchema()])
+      .getSchema()
       .openapi({ description: `Allowed columns are ${columns.join(', ')}`, example: [columns[0], columns[2]] }),
-    desc: new ArraySchema('asc', new StringSchema('asc').getSchema()).getSchema()
+    desc: new UnionSchema([new ArraySchema('desc', new StringSchema('desc').getSchema()).getSchema()
       .optional()
-      .transform((val) => !val ? [] : val)
+      .transform((val) => !val ? [] : val), new StringSchema('desc').getSchema()])
+      .getSchema()
       .openapi({ description: `Allowed columns are ${columns.join(', ')}`, example: [columns[1]] }),
   })
     .getSchema()
